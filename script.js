fc21ebade8250e67bd06fc7c4eaf4a139e85b752
Gaurav <gp43883@gmail.com>
2023-07-17 18:50:11 +0530

    All Designs are Changed

--- script.js
+++ script.js
@@ -1,44 +1,32 @@
-let clip = document.querySelector(".vid")
-    
-clip.addEventListener("mouseover", function (e) {
-        clip.play();
-        clip.style.cursor="pointer"
-})
-let clip1 = document.querySelector(".portClip")
-clip1.addEventListener("mouseover", function (e) {
-        clip1.play();
-        clip1.style.cursor="pointer"
-})
-let clip5 = document.querySelector(".eComClip")
-clip5.addEventListener("mouseover", function (e) {
-        clip5.play();
-        clip5.style.cursor="pointer"
-})
-let clip2 = document.querySelector(".digiClip")
-clip2.addEventListener("mouseover", function (e) {
-        clip2.play();
-        clip2.style.cursor="pointer"
-})
-let clip3 = document.querySelector(".blogClip")
-clip3.addEventListener("mouseover", function (e) {
-        clip3.play();
-        clip3.style.cursor="pointer"
-})
-let clip4 = document.querySelector(".courseClip")
-clip4.addEventListener("mouseover", function (e) {
-        clip4.play();
-        clip4.style.cursor="pointer"
+let docs1 = document.querySelector(".docs");
+let docs2 = document.querySelector(".project1Docs");
+let docs3 = document.querySelector(".project2Docs");
+let sub = document.querySelector("#submit");
+
+
+docs1.addEventListener('click', () =>{
+        alert("Documentation will be uploaded soon. Stay Tuned ;)");
 })
 
-let hamburger = document.querySelector(".hamburger")
-let nav = document.querySelector(".mobileNav")
-hamburger.addEventListener("click", ()=>{
-    if(nav.style.display === "block"){
-        nav.style.display = "none";
-    }
-
-    else{
-        nav.style.display = "block";
-        
-    }
+docs2.addEventListener('click', () =>{
+        alert("Documentation will be uploaded soon. Stay Tuned ;)");
 })
+
+docs3.addEventListener('click', () =>{
+        alert("Documentation will be uploaded soon. Stay Tuned ;)");
+})
+
+function afterClick(form){
+        alert("Thank you! Will connect soon!!");
+}
+
+function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.querySelector(".ham").style.display="none";
+      }
+      
+function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.querySelector(".ham").style.display = "block";
+      }
+
