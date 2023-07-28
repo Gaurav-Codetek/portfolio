c7e36e5606c9e909e86752f0ac14a43cf9bd71b7
Gaurav <gp43883@gmail.com>
2023-07-28 16:51:07 +0530

    Commit

--- public/js/script.js
+++ public/js/script.js
@@ -4,29 +4,31 @@ let docs3 = document.querySelector(".project2Docs");
 let sub = document.querySelector("#submit");
 
 
-docs1.addEventListener('click', () =>{
+docs1.addEventListener('click', () => {
         alert("Documentation will be uploaded soon. Stay Tuned ;)");
 })
 
-docs2.addEventListener('click', () =>{
+docs2.addEventListener('click', () => {
         alert("Documentation will be uploaded soon. Stay Tuned ;)");
 })
 
-docs3.addEventListener('click', () =>{
+docs3.addEventListener('click', () => {
         alert("Documentation will be uploaded soon. Stay Tuned ;)");
 })
 
-function afterClick(form){
+function afterClick(form) {
         alert("Thank you! Will connect soon!!");
 }
 
 function openNav() {
         document.getElementById("mySidenav").style.width = "250px";
-        document.querySelector(".ham").style.display="none";
-      }
-      
+        document.querySelector(".ham").style.display = "none";
+}
+
 function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
         document.querySelector(".ham").style.display = "block";
-      }
+}
+
+
 
